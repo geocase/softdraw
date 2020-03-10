8217db02df83858b1f1332a6d4dc7e3330e89285
Casey Georgeson <caseys.georgeson@gmail.com>
2020-03-09 22:11:42 -0700

    Update

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 #include "softdraw.h"
 #include "softdraw_image.h"
 
-#define WINDOW_X 320 
-#define WINDOW_Y 200
+#define WINDOW_X 1280 
+#define WINDOW_Y 720
 
 uint8_t running = 1;
 
@@ -23,14 +23,14 @@ int main() {
 	srand(time(NULL));
 
 	m_InitSDL();
-	Image_t *img = sdi_LoadBMP("face.bmp");
+	Image_t *img = sdi_LoadBMP("grad.bmp");
 	FrameBuffer_t *buf = sd_NewFrameBuffer(WINDOW_X, WINDOW_Y);	
 
 	while(running) {
 		sd_DrawLine(buf, 80, 150, mouseX, mouseY, SD_WHITE);	
 		sd_DrawLine(buf, 240, 150, mouseX, mouseY, SD_WHITE);	
 		sd_DrawLine(buf, 80, 150, 240, 150, SD_WHITE);	
-		sd_CopyTextureToBuffer(buf, img, mouseX, mouseY);
+		sd_CopyTextureToBuffer(buf, img, 0, 0);
 		m_BlitPixels(buf);
 		sd_ClearBuffer(buf, SD_GREEN);
 		m_Input();
